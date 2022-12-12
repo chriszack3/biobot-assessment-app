22d420334f32e30f74b707932d1a38c7e14cc6e1
chriszack3 <czack@chrispc.localdomain>
2022-12-12 16:44:39 -0500

    serve react app from express

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
