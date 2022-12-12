3a6fd240e79a29d31aae8432ee7ab956c003fce1
chriszack3 <czack@chrispc.localdomain>
2022-12-12 18:25:11 -0500

    respond with correct kits for autocomplete

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 const Form = () => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState({});
     const handleOnChange = async (e) => {
       
       fetch(`/api/${e?.target?.value}`)
-        .then((res) => res.json())
+        .then((res) => {
+            console.log(res)
+            return res.json()
+        })
         .then((data) => {
-            setData(data);
-            console.log(data);
-        });
-
+            console.log(data)
+            setData(data)
+        })
+        .catch((err) => console.log(err));
     }
   
     return (
       <div className="App">
      
         <input onChange={handleOnChange}></input>
+        {
+            data?.length > 0 && data.map(({label_id}, index) => {
+                return <div key={index}>{label_id}</div>
+            })
+        }
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const kits = require('./kits')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,8 +9,8 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api/*", (req, res) => {
-  
-  res.json({ message: `Hello from server! You requested: ${req.params[0]}`});
+  const autoComplete = kits.filter(kit => kit.label_id.includes(req.params[0]))
+  res.json(autoComplete);
 });
 
 // All other GET requests not handled before will return our React app
